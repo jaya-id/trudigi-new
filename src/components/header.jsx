41831142a023166566f3d19e703d208a1953a8b2
Afaisw <feizholreddevils@gmail.com>
2020-01-09 10:25:30 +0700

    just navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,6 +4,9 @@ export default () => (
   <header className="header">
 	<nav className="navbar navbar-expand-lg navbar-light bg-light">
 		<a className="navbar-brand" href="/">TRUDIGI</a>
+		<a className="navbar-brand" href="/showcase">Showcase</a>
+		<a className="navbar-brand" href="/">Products</a>
+		<a className="navbar-brand" href="/">Contact</a>
 	</nav>
   </header>
 )
