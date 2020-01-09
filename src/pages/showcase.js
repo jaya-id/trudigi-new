42ad463b78d6496c90a786131921278484d66465
Afaisw <feizholreddevils@gmail.com>
2020-01-09 10:17:25 +0700

    just content index showcase

--- src/pages/showcase.js
+++ src/pages/showcase.js
@@ -1,8 +1,28 @@
 import React from "react"
 import Layout from "../components/layout"
+import Summary from '../components/summary'
 
 export default () => (
 	<Layout>
-		Showcase
+		<Summary>
+			<h1>Contoh hasil pengerjaan oleh member TRUDIGI dalam berbagai bidang</h1>
+			<p>-</p>
+		</Summary>
+		<Summary>
+			<h1>ONLINE PROFILE</h1>
+			<p><img src="/images/template.jpg"></img></p>
+		</Summary>
+		<Summary>
+			<h1>ONLINE SYSTEM</h1>
+			<p><img src="/images/template.jpg"></img></p>
+		</Summary>
+		<Summary>
+			<h1>DIGITAL CONTENT</h1>
+			<p><img src="/images/template.jpg"></img></p>
+		</Summary>
+		<Summary>
+			<h1>DIGITAL GRAPHICS</h1>
+			<p><img src="/images/template.jpg"></img></p>
+		</Summary>
 	</Layout>
 )
