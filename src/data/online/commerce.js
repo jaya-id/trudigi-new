d1cd07eb1cc3d891f285aa18b2cd251f153053dd
Wildan M <willnode@wellosoft.net>
2020-01-18 12:15:06 +0700

    sorry broken build

--- src/data/online/commerce.js
+++ src/data/online/commerce.js
@@ -1,5 +1,9 @@
 
-//import items from '../shared/items'
+import items from '../shared/items'
+
+import promises from '../shared/promises'
+
+import technologies from '../shared/technologies'
 
 export default {
 	title: "Online Commerce",
@@ -7,5 +11,38 @@ export default {
 	background: "/images/template.jpg",
 	url: "/online/commerce",
 	theme: "warning",
-	models: []
+	models: [
+		{
+		name: "Mini",
+		summary: "Sistem untuk Menyimpan Data Pribadi",
+		price: 300000,
+		examples: [
+			{
+				title: "Aplikasi Kasir",
+				image: "/images/placeholder.png"
+			}, {
+				title: "Aplikasi Peminjaman",
+				image: "/images/placeholder.png"
+			},
+		],
+		items: [
+			items.itemWorkDuration(21),
+			items.itemRevisionPeriod(7),
+			items.onlineCRUD(3),
+			items.systemAdminKind('single'),
+		],
+		promises: [
+			promises.dynamic,
+			promises.sourceCode,
+			promises.responsive,
+			promises.security,
+		],
+		technologies: [
+			technologies.html5,
+			technologies.bootstrap,
+			technologies.codeIgniter,
+			technologies.mySql,
+		],
+	}
+]
 }
