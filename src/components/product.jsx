603f5b14446fe156049561ff6893ffa2adaaa171
Faisol W <55418262+Afaisw@users.noreply.github.com>
2020-02-24 22:04:25 +0700

    responsive fix
    Kurang Desktop yang navbarnya belum transparan

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default ({children }) => (
-    <div class="product-content bd-highlight">
+    <div class="product-content col-md-3 bd-highlight">
         {children}
     </div>
 )
--- src/components/sumproduct.jsx
+++ src/components/sumproduct.jsx
@@ -4,7 +4,7 @@ export default ({ children }) => (
   <section className={`hero-summary py-5 d-flex flex-column text`}>
 	  <div className="hero-content container p-3 text-center">
       <h1>Products</h1>
-      <div className="product-summary d-flex flex-row bd-highlight mb-3 container p-3 text-center">
+      <div className="product-summary d-flex flex-row bd-highlight mb-3 p-3 row text-center">
 		{children}
         </div>
 	  </div>
