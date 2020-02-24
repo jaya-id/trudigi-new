c1dfb23ec6147f64f46b05336020605a14f79707
Faisol W <55418262+Afaisw@users.noreply.github.com>
2020-02-24 21:42:01 +0700

    fixing responsive

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,7 +4,7 @@ export default ({ img, reversed, children }) => (
   <section className={`hero hero-image p-0 row no-gutters mb-5${reversed ? 'reversed' : ''}`}>
 	<div className="col-md-7 hero-img" style={{backgroundImage: `url(${img})`}}>
 	</div>
-	<div className="col-md-5 hero-content d-flex flex-column justify-content-center">
+	<div className="col-md-5 hero-content d-flex flex-column justify-content-center container p-5">
 	{children}
 	</div>
   </section>
